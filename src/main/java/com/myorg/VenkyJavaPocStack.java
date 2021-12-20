f077ab2c6b1adf71f4ffede117ee7ea8ba4468ae
venky23042021 <95252419+venky23042021@users.noreply.github.com>
2021-12-20 08:27:34 +0530

    Testing if pipeline gets triggered
    Adding a new comment to see if the pipeline gets triggered

--- src/main/java/com/myorg/VenkyJavaPocStack.java
+++ src/main/java/com/myorg/VenkyJavaPocStack.java
@@ -33,5 +33,7 @@ public class VenkyJavaPocStack extends Stack {
         final LambdaRestApi apigatewayPoc = LambdaRestApi.Builder.create(this, "Endpoint")
         .handler(venkyFunctionPOC)
         .build();
+        
+        //Adding a new comment to see if the pipeline gets triggered
     }
 }
